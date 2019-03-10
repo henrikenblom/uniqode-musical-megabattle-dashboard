99ffaf62effa3e875d1ec3cc4e25dd28a7620ca6
Henrik Enblom <henrik@enblom.com>
2019-03-10 17:21:15 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'uniqode-musical-megabattle-dashboard';
+}
